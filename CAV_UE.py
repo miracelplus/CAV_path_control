136d52d0ea9d310a48e10024600669a32fe6e445
Haowei Sun <printfplus@outlook.com>
2021-04-12 12:45:05 -0400

    update on UE SO result

--- CAV_UE.py
+++ CAV_UE.py
@@ -41,12 +41,15 @@ def CAV_UE_xy(myx, myy):
         System_time = System_time + 1000*(myx+myy-control_ratio*demand)
     return -System_time
 
-# def CAV_SO(cav_path_distribution):
-#     x = cp.Variable(road_num)
-#     SO_objective = cp.Minimize(cp.sum(cp.multiply(t0_list, x) + cp.multiply(t0_list, cp.multiply(0.15*cp.power(x, 5),ca_inverse_power_list))))
-#     constraints = [x >= cav_path_distribution, cp.sum(x) == demand]
-#     prob = cp.Problem(SO_objective, constraints)
-#     result = prob.solve()
-#     System_time = np.sum(t0_list * x.value + t0_list * 0.15 * x.value**5 * ca_inverse_power_list)
-#     return System_time
-    
+
+def CAV_UE_SO(mode="UE"):
+    x = cp.Variable(road_num)
+    UE_objective = cp.Minimize(cp.sum(cp.multiply(t0_list, x) + cp.multiply(t0_list, cp.multiply(0.03*cp.power(x, 5),ca_inverse_power_list))))
+    SO_objective = cp.Minimize(cp.sum(cp.multiply(t0_list, x) + cp.multiply(t0_list, cp.multiply(0.15*cp.power(x, 5),ca_inverse_power_list))))
+    constraints = [x >= 0, cp.sum(x) == demand]
+    if mode == "UE":
+        prob = cp.Problem(UE_objective, constraints)
+    else:
+        prob = cp.Problem(SO_objective, constraints)
+    result = prob.solve()
+    return result
